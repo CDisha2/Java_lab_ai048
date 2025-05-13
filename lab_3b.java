3bc2804832e9839e30efd39575ddb00396f9b395
C Disha <cdisha.ai23@bmsce.ac.in>
2025-05-13 15:19:56 +0530

    lab_3b.java

---
+++ lab_3b.java
@@ -0,0 +1,25 @@
+package D_BM23AI048;
+import java.util.Scanner;
+
+public class str_handling {
+    public static void main(String[] args){
+        //(i)
+        StringBuffer sb1=new StringBuffer();
+        System.out.println("capacity of the sb1:"+ sb1.capacity());
+        StringBuffer sb2= new StringBuffer("gello");
+        System.out.println("capacity of sb2:"+ sb2.capacity());
+
+        //(ii)
+        Scanner s= new Scanner(System.in);
+        System.out.println("enter str:");
+        String sb3in= s.nextLine();
+        StringBuffer sb3in2= new StringBuffer(sb3in);
+        String a=sb3in2.reverse().toString().toUpperCase();
+        System.out.println("reversed str in uppercase:"+ a);
+
+        //(iii)
+        System.out.println("enter the str:");
+        String ins=s.nextLine();
+        System.out.println("concatenated str:"+ a+ins);
+    }
+}
